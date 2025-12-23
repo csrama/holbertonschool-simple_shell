092abbd442bf87b8ad9f98441ed056471bf1a32a
csrama <ramadafer1@gmail.com>
2025-12-23 09:03:03 +0000

    Execute single-word commands using execve

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* External environment */
+extern char **environ;
+
+/* Function prototypes */
+void display_prompt(void);
+void execute_command(char *command);
+
+#endif /* SHELL_H */
