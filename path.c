2e87971735e6587ec0465bddf2375faa40073bd5
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-25 16:37:39 +0300

    Refactor get_path and improve function comments
    Refactor get_path function and update comments for clarity.

--- path.c
+++ path.c
@@ -6,31 +6,10 @@
 
 /**
 	* search_in_path - search command in PATH
-	* get_path - get the value of PATH from environment
-	* @env: environment variables
-	*
-	* Return: pointer to PATH string or NULL if not found
-	*/
-char *get_path(char **env)
-{
-	int i;
-	size_t len = 5;
-
-	for (i = 0; env[i]; i++)
-	{
-	if (strncmp(env[i], "PATH=", len) == 0)
-	return (env[i] + len);
-	}
-	return (NULL);
-}
-
-/**
-	* search_in_path - search command in a PATH string
-	* @cmd: command name
+		* @cmd: command name
 	* @path: PATH value
 	*
 	* Return: full path or NULL
-	* Return: full path string or NULL
 	*/
 static char *search_in_path(char *cmd, char *path)
 {
@@ -38,25 +17,21 @@ static char *search_in_path(char *cmd, char *path)
 	free(full);
 	token = strtok(NULL, ":");
 	}
-
 	free(copy);
 	return (NULL);
 }
 
 /**
 	* find_command - find executable using PATH
-	* find_command - find executable path of a command
 	* @cmd: command name
 	* @ctx: shell context
 	*
 	* Return: full path or NULL
-	* Return: full path string or NULL if not found
 	*/
 char *find_command(char *cmd, shell_ctx_t *ctx)
 {
 	struct stat st;
 	char *path;
-	struct stat st;
 
 	(void)ctx;
 
@@ -65,7 +40,6 @@ char *find_command(char *cmd, shell_ctx_t *ctx)
 	}
 
 	path = getenv("PATH");
-	path = get_path(ctx->env);
 	if (!path || path[0] == '\0')
 	return (NULL);
 
