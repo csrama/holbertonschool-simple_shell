13962315cc0f9ab9b2734a702a61ef4d502a425c
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-25 11:44:03 +0300

    Refactor shell.c for improved command execution
    Refactor shell.c to streamline command execution and input handling.

--- shell.c
+++ shell.c
@@ -2,216 +2,94 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 
-#define MAX_INPUT 1024
-#define MAX_ARGS 64
-#define MAX_PATH_LEN 4096
+extern char **environ;
 
-/**
- * split_string - Splits a string into tokens
- * @str: The string to split
- * @delim: The delimiter character
- * Return: Array of tokens, NULL terminated
- */
-char **split_string(char *str, const char *delim)
-{
-    char **tokens = malloc(MAX_ARGS * sizeof(char *));
-    char *token;
-    int i = 0;
-
-    if (!tokens)
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(str, delim);
-    while (token != NULL && i < MAX_ARGS - 1)
-    {
-        tokens[i] = token;
-        i++;
-        token = strtok(NULL, delim);
-    }
-    tokens[i] = NULL;
-    return tokens;
-}
-
-/**
- * get_path_dirs - Gets all directories in PATH
- * Return: Array of PATH directories, NULL terminated
- */
-char **get_path_dirs(void)
-{
-    char *path_env = getenv("PATH");
-    char *path_copy;
-    char **dirs;
-
-    if (!path_env)
-        return NULL;
-
-    path_copy = strdup(path_env);
-    if (!path_copy)
-    {
-        perror("strdup");
-        return NULL;
-    }
-
-    dirs = split_string(path_copy, ":");
-    free(path_copy);
-    return dirs;
-}
-
-/**
- * find_command_in_path - Searches for command in PATH directories
- * @command: The command to find
- * Return: Full path if found, NULL otherwise
- */
-char *find_command_in_path(char *command)
-{
-    char **path_dirs;
-    char *full_path = NULL;
-    struct stat st;
-    int i = 0;
-
-    /* If command contains '/', check it directly */
-    if (strchr(command, '/'))
-    {
-        if (stat(command, &st) == 0 && (st.st_mode & S_IXUSR))
-            return strdup(command);
-        return NULL;
-    }
-
-    path_dirs = get_path_dirs();
-    if (!path_dirs)
-        return NULL;
-
-    while (path_dirs[i])
-    {
-        full_path = malloc(strlen(path_dirs[i]) + strlen(command) + 2);
-        if (!full_path)
-        {
-            perror("malloc");
-            free(path_dirs);
-            return NULL;
-        }
-
-        sprintf(full_path, "%s/%s", path_dirs[i], command);
-
-        if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
-        {
-            free(path_dirs);
-            return full_path;
-        }
-
-        free(full_path);
-        i++;
-    }
-
-    free(path_dirs);
-    return NULL;
-}
-
-/**
- * execute_command - Executes a command with arguments
- * @args: Array of command and arguments
- * Return: 1 to continue, 0 to exit
- */
-int execute_command(char **args)
-{
-    pid_t pid;
-    int status;
-    char *full_path;
-
-    if (!args || !args[0])
-        return 1;
-
-    /* Handle exit command */
-    if (strcmp(args[0], "exit") == 0)
-        return 0;
-
-    /* Handle env command */
-    if (strcmp(args[0], "env") == 0)
-    {
-        extern char **environ;
-        char **env = environ;
-        while (*env)
-        {
-            printf("%s\n", *env);
-            env++;
-        }
-        return 1;
-    }
-
-    /* Find command in PATH */
-    full_path = find_command_in_path(args[0]);
-    if (!full_path)
-    {
-        fprintf(stderr, "./shell: No such file or directory\n");
-        return 1;
-    }
-
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        free(full_path);
-        return 1;
-    }
-
-    if (pid == 0)
-    {
-        /* Child process */
-        if (execve(full_path, args, environ) == -1)
-        {
-            perror("./shell");
-            free(full_path);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        /* Parent process */
-        waitpid(pid, &status, 0);
-        free(full_path);
-    }
-
-    return 1;
-}
-
-/**
- * main - Main shell loop
- * Return: Always 0
- */
 int main(void)
 {
-    char input[MAX_INPUT];
-    char **args;
-    int status = 1;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char *args[64];
+	int i, j;
+	pid_t pid;
+	int status;
 
-    while (status)
-    {
-        printf(":) ");
-        
-        if (!fgets(input, MAX_INPUT, stdin))
-        {
-            printf("\n");
-            break;
-        }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-        /* Remove newline character */
-        input[strcspn(input, "\n")] = 0;
+		read = getline(&line, &len, stdin);
 
-        /* Skip empty input */
-        if (strlen(input) == 0)
-            continue;
+		if (read == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			free(line);
+			exit(0);
+		}
 
-        args = split_string(input, " ");
-        status = execute_command(args);
-        free(args);
-    }
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
 
-    return 0;
+		/* Check if line has any non-space characters */
+		for (i = 0; line[i] != '\0'; i++)
+			if (line[i] != ' ' && line[i] != '\t')
+				break;
+		
+		if (line[i] == '\0')
+			continue;
+
+		/* Split into arguments */
+		i = 0;
+		j = 0;
+		while (line[j] != '\0')
+		{
+			/* Skip spaces */
+			while (line[j] == ' ' || line[j] == '\t')
+				j++;
+			
+			if (line[j] == '\0')
+				break;
+			
+			args[i] = &line[j];
+			i++;
+			
+			/* Find end of argument */
+			while (line[j] != '\0' && line[j] != ' ' && line[j] != '\t')
+				j++;
+			
+			if (line[j] != '\0')
+			{
+				line[j] = '\0';
+				j++;
+			}
+		}
+		args[i] = NULL;
+
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			continue;
+		}
+
+		if (pid == 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+				fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+				exit(127);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+
+	free(line);
+	return (0);
 }
