724238a3f49055bb2241e26f72094efbf7bbcb87
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-25 12:44:34 +0300

    Remove find_command function and simplify main logic

--- shell.c
+++ shell.c
@@ -6,75 +6,12 @@
 
 extern char **environ;
 
-/**
- * find_command - find command in PATH
- * @cmd: command name
- * Return: full path or NULL
- */
-char *find_command(char *cmd)
-{
-	char *path, *path_copy, *dir, *full_path;
-	int cmd_len, dir_len;
-
-	if (!cmd)
-		return (NULL);
-
-	/* Absolute or relative path */
-	if (cmd[0] == '/' || cmd[0] == '.')
-	{
-		if (access(cmd, X_OK) == 0)
-			return (strdup(cmd));
-		return (NULL);
-	}
-
-	path = getenv("PATH");
-	if (!path || path[0] == '\0')
-		return (NULL);
-
-	path_copy = strdup(path);
-	if (!path_copy)
-		return (NULL);
-
-	cmd_len = strlen(cmd);
-	dir = strtok(path_copy, ":");
-
-	while (dir)
-	{
-		dir_len = strlen(dir);
-		full_path = malloc(dir_len + cmd_len + 2);
-		if (!full_path)
-		{
-			free(path_copy);
-			return (NULL);
-		}
-
-		sprintf(full_path, "%s/%s", dir, cmd);
-
-		if (access(full_path, X_OK) == 0)
-		{
-			free(path_copy);
-			return (full_path);
-		}
-
-		free(full_path);
-		dir = strtok(NULL, ":");
-	}
-
-	free(path_copy);
-	return (NULL);
-}
-
-/**
- * main - simple shell
- * Return: 0
- */
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
 	char *args[64];
-	char *cmd_path;
 	int i, j;
 	pid_t pid;
 	int status;
@@ -85,6 +22,7 @@ int main(void)
 			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
 		read = getline(&line, &len, stdin);
+
 		if (read == -1)
 		{
 			if (isatty(STDIN_FILENO))
@@ -96,52 +34,60 @@ int main(void)
 		if (line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
-		/* Ignore empty lines */
-		for (i = 0; line[i]; i++)
+		/* Check if line has any non-space characters */
+		for (i = 0; line[i] != '\0'; i++)
 			if (line[i] != ' ' && line[i] != '\t')
 				break;
-		if (!line[i])
+		
+		if (line[i] == '\0')
 			continue;
 
-		/* Split input into arguments */
+		/* Split into arguments */
 		i = 0;
 		j = 0;
-		while (line[j])
+		while (line[j] != '\0')
 		{
+			/* Skip spaces */
 			while (line[j] == ' ' || line[j] == '\t')
 				j++;
-			if (!line[j])
+			
+			if (line[j] == '\0')
 				break;
-
-			args[i++] = &line[j];
-			while (line[j] && line[j] != ' ' && line[j] != '\t')
+			
+			args[i] = &line[j];
+			i++;
+			
+			/* Find end of argument */
+			while (line[j] != '\0' && line[j] != ' ' && line[j] != '\t')
 				j++;
-			if (line[j])
-				line[j++] = '\0';
+			
+			if (line[j] != '\0')
+			{
+				line[j] = '\0';
+				j++;
+			}
 		}
 		args[i] = NULL;
 
-		/* Find command */
-		cmd_path = find_command(args[0]);
-		if (!cmd_path)
+		pid = fork();
+		if (pid < 0)
 		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-			continue; /* 🚫 NO fork */
+			perror("fork");
+			continue;
 		}
 
-		pid = fork();
 		if (pid == 0)
 		{
-			execve(cmd_path, args, environ);
-			perror("execve");
-			exit(1);
+			if (execve(args[0], args, environ) == -1)
+			{
+				fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+				exit(127);
+			}
 		}
-		else if (pid > 0)
+		else
 		{
 			wait(&status);
 		}
-
-		free(cmd_path);
 	}
 
 	free(line);
