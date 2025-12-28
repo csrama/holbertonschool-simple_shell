76725300036d1028a2572ec57adce2cde4473bba
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-28 09:19:50 +0000

    Update execute_command with PATH verification

--- shell.h
+++ shell.h
@@ -1,21 +1,48 @@
-#ifndef SHELL_H
-#define SHELL_H
+#include "shell.h"
+#include "main.h"
 
-/* Libraries */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <ctype.h>  
+/* Add this function or modify existing execute_command */
+int execute_command(char **args)
+{
+    pid_t pid;
+    int status;
+    char *full_path;
 
-/* External environment */
-extern char **environ;
+    /* Check if command exists before forking */
+    full_path = find_command_in_path(args[0]);
+    if (!full_path)
+    {
+        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+        return (127); /* Command not found */
+    }
 
-/* Function prototypes */
-void display_prompt(void);
-void execute_command(char *command);
-char *trim_spaces(char *str);  
+    /* Now we can safely fork */
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        free(full_path);
+        return (1);
+    }
+    else if (pid == 0) /* Child */
+    {
+        if (execve(full_path, args, environ) == -1)
+        {
+            perror("execve");
+            free(full_path);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else /* Parent */
+    {
+        waitpid(pid, &status, 0);
+        free(full_path);
+        
+        if (WIFEXITED(status))
+            return (WEXITSTATUS(status));
+        else
+            return (1);
+    }
 
-#endif /* SHELL_H */
+    return (0);
+}
