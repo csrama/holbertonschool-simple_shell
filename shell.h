09eee20e9b016f8b3916a089759a96f9aee2768e
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-28 11:01:20 +0000

    Create unified header with includes, macros, and function prototypes

--- shell.h
+++ shell.h
@@ -1,15 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
 
 extern char **environ;
+
+int execute_command(char **args);
+char *find_path(char *command);
+char *get_path(void);
+
 extern char *prog_name;
 extern unsigned int line_number;
-
-char *get_path(void);
-char *find_path(char *command);
-int execute_command(char **args);
 
 #endif
 
