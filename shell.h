4d75b5cbf258dba9c4d2515319e1ff0bf8bf156d
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-28 09:22:23 +0000

    Update shell.h with PATH-related function prototypes

--- shell.h
+++ shell.h
@@ -1,48 +1,9 @@
-#include "shell.h"
-#include "main.h"
+#ifndef SHELL_H
+#define SHELL_H
 
-/* Add this function or modify existing execute_command */
-int execute_command(char **args)
-{
-    pid_t pid;
-    int status;
-    char *full_path;
+/* Add these declarations if not already present */
+char *find_command_in_path(char *command);
+int check_command_exists(char *command);
+int execute_command(char **args);
 
-    /* Check if command exists before forking */
-    full_path = find_command_in_path(args[0]);
-    if (!full_path)
-    {
-        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-        return (127); /* Command not found */
-    }
-
-    /* Now we can safely fork */
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        free(full_path);
-        return (1);
-    }
-    else if (pid == 0) /* Child */
-    {
-        if (execve(full_path, args, environ) == -1)
-        {
-            perror("execve");
-            free(full_path);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else /* Parent */
-    {
-        waitpid(pid, &status, 0);
-        free(full_path);
-        
-        if (WIFEXITED(status))
-            return (WEXITSTATUS(status));
-        else
-            return (1);
-    }
-
-    return (0);
-}
+#endif /* SHELL_H */
