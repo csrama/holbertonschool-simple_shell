1703bbcd05ebcf66d2c05f7aeea1fd5669f63967
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-28 10:59:56 +0000

    Fix PATH handling: no fork for non-existent commands, handle empty PATH

--- path.c
+++ path.c
@@ -1,70 +1,66 @@
 #include "shell.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <unistd.h>
+#include <stdio.h>
 
-/* get PATH environment variable */
+/* get PATH from environment */
 char *get_path(void)
 {
-	int i;
+    int i = 0;
 
-	if (!environ)
-		return NULL;
+    if (!environ)
+        return NULL;
 
-	for (i = 0; environ[i]; i++)
-	{
-		if (strncmp(environ[i], "PATH=", 5) == 0)
-			return environ[i] + 5;
-	}
-	return NULL;
+    while (environ[i])
+    {
+        if (strncmp(environ[i], "PATH=", 5) == 0)
+            return environ[i] + 5;
+        i++;
+    }
+    return NULL;
 }
 
-/* find full path of a command */
+/* find full path of command */
 char *find_path(char *command)
 {
-	char *path, *path_copy, *dir, *full;
-	struct stat st;
+    char *path = get_path();
+    char *path_copy, *dir;
+    char *full;
+    struct stat st;
 
-	if (strchr(command, '/'))
-	{
-		if (stat(command, &st) == 0)
-			return strdup(command);
-		return NULL;
-	}
+    if (!path)
+        return NULL;
 
-	path = get_path();
-	if (!path || strlen(path) == 0)
-		return NULL;
+    if (strchr(command, '/'))
+    {
+        if (stat(command, &st) == 0)
+            return strdup(command);
+        else
+            return NULL;
+    }
 
-	path_copy = strdup(path);
-	if (!path_copy)
-		return NULL;
+    path_copy = strdup(path);
+    if (!path_copy)
+        return NULL;
 
-	dir = strtok(path_copy, ":");
-	while (dir)
-	{
-		full = malloc(strlen(dir) + strlen(command) + 2);
-		if (!full)
-		{
-			free(path_copy);
-			return NULL;
-		}
+    dir = strtok(path_copy, ":");
+    while (dir)
+    {
+        full = malloc(strlen(dir) + strlen(command) + 2);
+        if (!full)
+            return NULL;
+        sprintf(full, "%s/%s", dir, command);
+        if (stat(full, &st) == 0)
+        {
+            free(path_copy);
+            return full;
+        }
+        free(full);
+        dir = strtok(NULL, ":");
+    }
 
-		sprintf(full, "%s/%s", dir, command);
-
-		if (stat(full, &st) == 0)
-		{
-			free(path_copy);
-			return full;
-		}
-
-		free(full);
-		dir = strtok(NULL, ":");
-	}
-
-	free(path_copy);
-	return NULL;
+    free(path_copy);
+    return NULL;
 }
 
