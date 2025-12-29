77734220322dc8d2453e88e09064a4d769bae63d
csrama <ramadafer1@gmail.com>
2025-12-29 09:02:22 +0000

    trying a new approach and inserting

--- shell.c
+++ shell.c
@@ -4,32 +4,41 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 
+/* Execute a command */
 int execute_command(char **args)
 {
     pid_t pid;
     char *cmd_path;
-    char *path;
 
     if (!args || !args[0])
         return 0;
 
-    path = get_path_value();
-    if (path && path[0] == '\0')
+    /* If command contains '/' -> treat it as path, do NOT search PATH */
+    if (strchr(args[0], '/'))
     {
-        fprintf(stderr, "%s: %u: %s: not found\n",
-                prog_name, line_number, args[0]);
-        return 127;
+        if (access(args[0], X_OK) != 0)
+        {
+            fprintf(stderr, "%s: %u: %s: not found\n",
+                    prog_name, line_number, args[0]);
+            return 127;
+        }
+        cmd_path = strdup(args[0]);
+    }
+    else
+    {
+        /* Search PATH for the command */
+        cmd_path = find_path(args[0]);
+        if (!cmd_path)
+        {
+            fprintf(stderr, "%s: %u: %s: not found\n",
+                    prog_name, line_number, args[0]);
+            return 127;
+        }
     }
 
-    cmd_path = find_path(args[0]);
-    if (!cmd_path)
-    {
-        fprintf(stderr, "%s: %u: %s: not found\n",
-                prog_name, line_number, args[0]);
-        return 127;
-    }
-
+    /* Fork and execute */
     pid = fork();
     if (pid == -1)
     {
